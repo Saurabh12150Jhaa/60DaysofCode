f18347e8952a95a2507938cb119d75f250391bbc
Saurabh Jha <saurabh12150jha@gmail.com>
2023-05-16 23:23:13 +0530

    //Product of Array Except Self

---
+++ Product of Array Except Self.java
@@ -0,0 +1,17 @@
+//Product of Array Except Self
+//Given an array nums of n integers where n > 1,  return an array output such that output[i] is equal to the product of all the elements of nums except nums[i].
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        int[] result = new int[nums.length];
+        result[0] = 1;
+        for (int i = 1; i < nums.length; i++) {
+            result[i] = result[i - 1] * nums[i - 1];
+        }
+        int right = 1;
+        for (int i = nums.length - 1; i >= 0; i--) {
+            result[i] *= right;
+            right *= nums[i];
+        }
+        return result; 
+    }
+}
